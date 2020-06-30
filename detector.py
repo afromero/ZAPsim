40aff452850e7bd5785689a7a5eea22c269ee566
Romero-Wolf <rw.adres@gmail.com>
2020-06-30 16:25:42 -0700

    initial commit

---
+++ detector.py
@@ -0,0 +1,283 @@
+from nuRadioEmission import *
+#rcParams['font.size']=14
+nuRFEm = nuRadioEmission('moon')
+from scipy.interpolate import interp1d
+import numpy as np
+
+def galactic_temperature(f_MHz):
+    # Dulk 2001
+    speed_of_light = 299792.458 # km / second
+    nu = f_MHz # Hz
+    tau = 5.0 * pow(nu, -2.1)
+    Ieg = 1.0610e-20
+    Ig  = 2.48e-20
+    # Iv in  W/m^2/Hz/sr
+    Iv = Ig * pow(nu, -0.52) * (1-np.exp(-tau))/tau + Ieg * pow(nu, -0.80) * np.exp(-tau)
+
+    kB = 1.38064852e-23 # Watts / Hz / K
+    c = speed_of_light * 1e3 # m/s
+    temp = Iv * c**2 / (2*(nu*1e6)**2)/kB
+    return Iv, temp # W/m^2/Hz/sr, K
+
+class Detector:
+    '''
+    Frequencies are in MHz
+    '''
+    def __init__(self, geom, freq_lo=30., freq_hi=300., 
+                 h_ant=1.0, a_ant=0.5e-2, 
+                 T_amp = 500., ZL = 50.,
+                 FE_type = 0, # 0 = transformer, 1 = resistive FET. 
+                 impedance_ratio = 1., # for transformers these are typically square integer ratios
+                 amp_noise_voltage = 1.2e-9, # V/sqrt(Hz) for FETs 
+                 Nfrq=100, th_inc_cut_deg = 38.):
+        '''
+        Constants
+        '''
+        self.kB      = 1.38064852e-23 # Watts / Hz / K
+        self.c_m_MHz = 299.792458 # speed of light in meter-MHz
+        self.Z0      = 120.*np.pi # Ohm
+        '''
+        This cut needs to be in refracted angle
+        '''
+        self.th_inc_cut_deg = th_inc_cut_deg
+
+        '''
+        Detector Variables
+        '''
+        self.freq_lo = freq_lo
+        self.freq_hi = freq_hi
+        self.h_ant   = h_ant 
+        self.T_amp   = T_amp
+        self.a_ant   = a_ant 
+        self.ZL      = ZL
+        self.Nfrq    = Nfrq
+        self.impedance_ratio = impedance_ratio
+
+        '''
+        Initialize frequency array
+        '''
+        self.freq      = np.linspace(self.freq_lo, self.freq_hi, Nfrq, endpoint=True) # in MHz
+        self.Nfrqs     = len(self.freq)
+        self.df        = self.freq[1] - self.freq[0]
+        self.lam_array = self.c_m_MHz / self.freq # in meters
+
+        '''
+        Initialize data arrays
+        '''
+        self.RE       = np.zeros((self.Nfrqs, geom.num_events))
+        self.att_len  = np.zeros((self.Nfrqs, geom.num_events))
+        self.att      = np.zeros((self.Nfrqs, geom.num_events))
+        self.E_reg    = np.zeros((3, self.Nfrqs, geom.num_events))
+        self.E_det    = np.zeros((3, self.Nfrqs, geom.num_events))
+        self.V_det    = np.zeros((3, self.Nfrqs, geom.num_events))
+        self.V_pk     = np.zeros((3, geom.num_events))
+        self.V_gal_sq = np.zeros(self.Nfrqs)
+        
+        
+        '''
+        Initialize auxiliary inputs
+        '''
+        self.E_eV = 10**geom.proton_log10_energy
+        self.energy_MeV = self.E_eV*1.e-6
+        
+        B_gal, self.T_gal = galactic_temperature(self.freq)
+        #print T_gal.shape
+        
+        '''
+        Amplifier noise contribution in V^2/MHz
+        '''
+        self.V_amp_sq = (self.kB*1.e6) * self.T_amp * np.real(self.ZL) * self.impedance_ratio # Noise voltage referenced to 50 Ohms
+        if FE_type == 1:
+            self.V_amp_sq = amp_noise_voltage**2  * 1.e6 # The factor of 1.e6 to is convert from V/sqrt(Hz) to V/sqrt(MHz) and squared.
+
+        self.get_loss_tan(geom)
+        
+        
+        #self.R_ant     = 20. * pi**2 * (self.h_ant/self.lam_array)**2
+        #self.X_ant     = -120. * self.lam_array / np.pi / self.h_ant * (np.log(self.h_ant/2./self.a_ant)-1.)
+        f_ant    = []
+        R_ant    = []
+        X_ant    = []
+        maxD_ant = []
+        '''Read Antenna Data (1m dipole, 1mm diameter rod)'''
+        h_ant_string = '%1.0fm'%(h_ant)
+        if h_ant%1.0 !=0.:         
+            h_ant_string = '%1.1fm'%(h_ant)
+            h_ant_string = h_ant_string.replace('.','p')
+
+        fnm = 'dipole_data_%s.txt'%h_ant_string
+        print 'dipole file name:', fnm
+        for line in file(fnm):
+            f_ant.append(float(line.split()[0]))
+            R_ant.append(float(line.split()[1]))
+            X_ant.append(float(line.split()[2]))
+            maxD_ant.append(float(line.split()[3]))
+        
+        iRa = interp1d(f_ant, R_ant)    
+        iXa = interp1d(f_ant, X_ant)    
+        imD = interp1d(f_ant, maxD_ant)
+        
+        self.R_ant    = iRa(self.freq)
+        self.X_ant    = iXa(self.freq)
+        self.maxD_ant = imD(self.freq)
+
+        #self.V_div_fac = np.abs(self.ZL)/np.abs(self.ZL + self.R_ant + 1j*self.X_ant)
+        '''NOTE THIS HAS TO GET SORTED OUT'''
+        '''
+        The electrically short antenna architecture depends on the amplifier noise and the voltage divider term. 
+        '''
+        self.V_div_fac = np.abs(self.ZL)**2/np.abs(self.ZL + self.R_ant + 1j*self.X_ant)**2
+        if FE_type == 0:
+            if np.abs(self.ZL-50.)>0.1 : 
+                print '!!!! WARNING !!!!'
+                print 'Using a transformer with a non-50 Ohm amplifier is unusual'
+                print 'You have been warned.'
+                print '!!!! WARNING !!!!'
+            print 'Calculating transformed impedance'
+            ZL_tran = self.ZL * self.impedance_ratio
+            self.V_div_fac = np.abs(ZL_tran)**2/np.abs(ZL_tran + self.R_ant + 1j*self.X_ant)**2
+            
+        self.h_eff     = np.sqrt(4.* self.V_div_fac * self.R_ant/self.Z0 * self.lam_array**2/4./np.pi)
+        
+        '''
+        Galactic noise spectrum in V^2/MHz
+        '''
+        self.V_gal_sq_spec = self.V_div_fac * 4. * self.R_ant * (self.kB*1.e6) * self.T_gal #*df
+        self.V_rms         = np.sqrt(np.sum(self.V_gal_sq_spec + self.V_amp_sq) * self.df)
+
+        '''
+        Effective height unit vectors
+        '''
+        r_hat = -geom.u_orb # direction of the radiation
+        
+        x_hat = np.zeros(r_hat.shape)
+        y_hat = np.zeros(r_hat.shape)
+        z_hat = np.zeros(r_hat.shape)
+        
+        # rotate the x-y plane randomly
+        self.ant_az = np.random.uniform(0., 2.*np.pi, geom.num_events)
+
+        x_hat[0,:] = +np.cos(self.ant_az)   
+        x_hat[1,:] = +np.sin(self.ant_az)  
+        y_hat[0,:] = -np.sin(self.ant_az) 
+        y_hat[1,:] = +np.cos(self.ant_az) 
+        z_hat[2,:] = 1.
+        
+        
+        h_hat_x = x_hat - np.einsum('ij,ij->j',x_hat, r_hat)*r_hat
+        h_hat_y = y_hat - np.einsum('ij,ij->j',y_hat, r_hat)*r_hat
+        h_hat_z = z_hat - np.einsum('ij,ij->j',z_hat, r_hat)*r_hat
+        
+        h_hat_x /= np.einsum('ij,ij->j',h_hat_x, h_hat_x)
+        h_hat_y /= np.einsum('ij,ij->j',h_hat_y, h_hat_y)
+        h_hat_z /= np.einsum('ij,ij->j',h_hat_z, h_hat_z)
+        
+        # cosine along each direction
+        cos_th_x = np.einsum('ij,ij->j', x_hat, r_hat)
+        cos_th_y = np.einsum('ij,ij->j', y_hat, r_hat)
+        cos_th_z = np.einsum('ij,ij->j', z_hat, r_hat)
+        '''
+        Frequency loop
+        '''
+        for k in range(self.Nfrqs):
+            self.RE[k,:]      = nuRFEm.AMVZ(1., self.freq[k], geom.view_angle_rad, self.energy_MeV)
+            self.att_len[k,:] = (self.lam_array[k]/(np.pi*geom.n_reg*self.loss_tan))
+            self.att[k,:]     = np.exp(-geom.dist_reg*1.e3/self.att_len[k,:])
+            
+            self.E_reg[0,k,:] = self.RE[k,:]/(geom.dist_reg*1.e3)*self.att[k,:]*(geom.pol_vec_reg[0])
+            self.E_reg[1,k,:] = self.RE[k,:]/(geom.dist_reg*1.e3)*self.att[k,:]*(geom.pol_vec_reg[1])
+            self.E_reg[2,k,:] = self.RE[k,:]/(geom.dist_reg*1.e3)*self.att[k,:]*(geom.pol_vec_reg[2])
+
+            self.E_det[0,k,:] = self.RE[k,:]*self.att[k,:] * (geom.e_perp * geom.u_pol_vec_perp_ref[0,:] + geom.e_para * geom.u_pol_vec_para_ref[0,:])
+            self.E_det[1,k,:] = self.RE[k,:]*self.att[k,:] * (geom.e_perp * geom.u_pol_vec_perp_ref[1,:] + geom.e_para * geom.u_pol_vec_para_ref[1,:])
+            self.E_det[2,k,:] = self.RE[k,:]*self.att[k,:] * (geom.e_perp * geom.u_pol_vec_perp_ref[2,:] + geom.e_para * geom.u_pol_vec_para_ref[2,:])
+
+            '''
+            First term is the E-field.
+            Second term is the "sin theta" part of the radiation pattern.
+            '''
+            ''' TO DO: The x-y antennas need to sample a random azimuth'''
+            # h = h_eff * D(\theta)
+            # V = E \dot h_hat
+            
+            # Directivity (short dipole, update to general dipole formula)
+            '''Short Dipole'''
+            #Dx = 1.5*(1.-cos_th_x**2)
+            #Dy = 1.5*(1.-cos_th_y**2)
+            #Dz = 1.5*(1.-cos_th_z**2)
+            
+            ''' General Dipole, normalized using NEC2 outputs '''
+            kL = (2.*np.pi/self.lam_array[k])*self.h_ant
+            Dx = (np.cos(0.5*kL*cos_th_x)-np.cos(0.5*kL))**2/(1.-cos_th_x**2)
+            Dy = (np.cos(0.5*kL*cos_th_y)-np.cos(0.5*kL))**2/(1.-cos_th_y**2)
+            Dz = (np.cos(0.5*kL*cos_th_z)-np.cos(0.5*kL))**2/(1.-cos_th_z**2)
+            
+            norm = 10**(self.maxD_ant[k]/10.)/(1.-np.cos(0.5*kL))**2
+            Dx *= norm
+            Dy *= norm
+            Dz *= norm
+            
+            #plot(cos_th_z, Dz, '.')
+            
+            V_x = np.einsum('ij,ij->j',self.E_det[:,k,:], h_hat_x) * self.h_eff[k] * np.sqrt(Dx)
+            V_y = np.einsum('ij,ij->j',self.E_det[:,k,:], h_hat_y) * self.h_eff[k] * np.sqrt(Dy)
+            V_z = np.einsum('ij,ij->j',self.E_det[:,k,:], h_hat_z) * self.h_eff[k] * np.sqrt(Dz)
+            
+            #self.V_det[0,k,:] = (np.cos(self.ant_az)*self.E_det[0,k,:] - np.sin(self.ant_az)*self.E_det[1,k,:]) * self.h_eff[k]
+            #self.V_det[1,k,:] = (np.sin(self.ant_az)*self.E_det[0,k,:] + np.cos(self.ant_az)*self.E_det[1,k,:]) * self.h_eff[k]
+            #self.V_det[2,k,:] = self.E_det[2,k,:] * self.h_eff[k]
+            
+            #self.V_det[0,k,:] = np.cos(self.ant_az) * V_x - np.sin(self.ant_az) * V_y
+            #self.V_det[1,k,:] = np.sin(self.ant_az) * V_x + np.cos(self.ant_az) * V_y
+            #self.V_det[2,k,:] = V_z
+
+            self.V_det[0,k,:] = V_x 
+            self.V_det[1,k,:] = V_y
+            self.V_det[2,k,:] = V_z
+
+            self.V_pk[0,:] += self.V_det[0,k,:]*self.df
+            self.V_pk[1,:] += self.V_det[1,k,:]*self.df
+            self.V_pk[2,:] += self.V_det[2,k,:]*self.df
+            '''
+            E_pk *= att**(1./lam_array[k])
+            E_perp = E_pk * geom.e_perp * geom.u_pol_vec_perp_ref 
+            E_para = E_pk * geom.e_para * geom.u_pol_vec_para_ref
+
+            #V_pk[0,:] += np.sqrt(fac[k] * 4.*R_ant[k]/Z0 * lam_array[k]**2/4./pi) * E_pk * geom.e_perp * df
+            #V_pk[1,:] += np.sqrt(fac[k] * 4.*R_ant[k]/Z0 * lam_array[k]**2/4./pi) * E_pk * geom.e_para * df
+            V_pk[0,:] += np.sqrt(fac[k] * 4.*R_ant[k]/Z0 * lam_array[k]**2/4./pi) * (E_perp[0,:] + E_para[0,:]) 
+            V_pk[1,:] += np.sqrt(fac[k] * 4.*R_ant[k]/Z0 * lam_array[k]**2/4./pi) * (E_perp[1,:] + E_para[1,:]) 
+            V_pk[2,:] += np.sqrt(fac[k] * 4.*R_ant[k]/Z0 * lam_array[k]**2/4./pi) * (E_perp[2,:] + E_para[2,:]) 
+            '''
+
+        trig1 = geom.th_inc_rad*180./np.pi<self.th_inc_cut_deg  
+        trig2 = np.logical_or(self.V_pk[0]>5.*self.V_rms, self.V_pk[1]>5.*self.V_rms)
+        trig2 = np.logical_or(trig2, self.V_pk[2]>5.*self.V_rms)
+
+        self.trig = np.logical_and(trig1, trig2)
+    '''
+    Function to initizalize the loss tangents for each event.
+    '''
+    def get_loss_tan(self, geom):
+        '''
+        Use a simple distribution of the Oxide fractions in the regolith (Mare, farside, etc.)
+        '''
+        self.M = np.zeros(geom.num_events) # this is %TiO2 + %FeO
+        self.M[0:int(0.6*geom.num_events)] = 5.
+        u = np.arange(int(0.6*geom.num_events), geom.num_events)
+        self.M[int(0.6*geom.num_events):geom.num_events] = 5.+30.*(u-np.min(u))/(np.max(u)-np.min(u))
+        '''
+        Randomize the events Mare and highlands position of the events.
+        '''
+        np.random.shuffle(self.M)
+        '''
+        Estimate the loss tangents using the parameterization in the Lunar Source Book
+        Note that this is the regolith density expected at a depth of a couple of meters. 
+        It does vary, which is something that will have to be treated in a more sophisticated Monte Carlo.
+        '''
+        self.loss_tan = np.power(10., 0.038*self.M + 0.312*geom.density_reg - 3.26)
+        #L_att_div_lam = (np.pi*(self.n_reg)*loss_tan)**-1.
+        #att = np.exp(-geom.dist_reg*1.e3/L_att_div_lam)
+        
+        
+        
---
+++ geometry.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 21 17:06:16 2018
+
+@author: romerowo
+"""
+
+import numpy as np
+from scipy.interpolate import Akima1DInterpolator
+from scipy.optimize import minimize_scalar
+from scipy.optimize import minimize
+
+
+class Geometry:
+    def __init__(self, altitude= 50., proton_log10_energy = 20., num_events=1000):
+        self.altitude = altitude
+        self.num_events = num_events
+        self.proton_log10_energy = proton_log10_energy
+        
+        ''' Define the radius of the Moon '''
+        self.Moon_radius_km = 1737.1
+        
+        '''
+        The density and index of refraction of the regolith have a gradient 
+        (see Lunar Source book)
+        This will have to be treated in a more sophisticated Monte Carlo.
+        Note: Alvarez-Muniz+ 2006 uses a density of 1.8 g/cm^3
+        We are using a density of 1.4 g/cm^3 because this is relevant to shower
+        development of UHECRs.
+        We will have to update the radio emission model for the relevant 
+        densities and index of refraction of regolith. 
+        '''
+        
+        self.density_reg = 1.4 # g/cm^3
+        
+        '''
+        The index of refraction relevant here is at the surface. 
+        Alvarez-Muniz et al. used 1.73 but that is for deep regolith. The
+        value relevant for refraction is 1.55 near the surface
+        '''
+        self.n_reg = 1.55 # index of refraction of the lunar regolith
+        
+        '''
+        The geometric factor is obtained from integrating the acceptance 
+        assuming every event is visible. This is the scaling factor for the
+        efficiency.
+        '''
+        
+        self.AOmega_0 = 2.*np.pi**2 * self.Moon_radius_km * self.altitude / (1.+self.altitude/self.Moon_radius_km) # km^2 sr
+
+        ''' Calculate the horizon latitude angle '''
+        
+        self.cos_horz = (1.+self.altitude/self.Moon_radius_km)**(-1)
+        
+        '''Get random interaction points'''
+        
+        self.cos_th_M = np.random.uniform(self.cos_horz, 1., self.num_events)
+        self.sin_th_M = np.sqrt(1.-self.cos_th_M**2)
+        
+        ''' The entry point of the cosmic ray in Cartesian coordinates '''
+        self.r_M = np.zeros((3, self.num_events))
+        self.r_M[0,:] = self.sin_th_M 
+        self.r_M[2,:] = self.cos_th_M 
+
+        
+        '''
+        Get random directions.
+        Note, we sample the cosmic ray zenith angle in cos^2 (not cos) to 
+        take the dot product in the integral into account. This angle is 
+        defined in a coordinate system with the z-axis along the normal to 
+        the surface of the Moon.
+        '''
+        self.cos_th_CR = np.sqrt(np.random.uniform(0., 1., self.num_events))
+        self.phi_CR    = np.random.uniform(0., 2.*np.pi, self.num_events)
+
+        '''
+        This is the direction unit vector of the particle  in a coordinate 
+        system with the z-axis defined along the direction of the normal to 
+        the Moon. 
+        '''
+        k_x = np.sqrt(1.-self.cos_th_CR**2) * np.cos(self.phi_CR)
+        k_y = np.sqrt(1.-self.cos_th_CR**2) * np.sin(self.phi_CR)
+        k_z = self.cos_th_CR
+        
+        '''
+        This is the direction unit vector in the coordinate system of the 
+        spacecraft (S/C is in the z-axis of the Moon)
+        '''
+        self.r_CR = np.zeros((3, self.num_events))
+        self.r_CR[0,:] = +k_x*self.cos_th_M + k_z*self.sin_th_M
+        self.r_CR[1,:] =  k_y
+        self.r_CR[2,:] = -k_x*self.sin_th_M + k_z*self.cos_th_M
+        
+        '''
+        Calculate the zenith angle of the event w.r.t. the normal to the surface 
+        '''
+        #self.cos_theta_zen =  self.r_CR[0,:]*self.r_M[0,:] \
+        #                    + self.r_CR[1,:]*self.r_M[1,:] \
+        #                    + self.r_CR[2,:]*self.r_M[2,:] 
+                            
+        self.proton_log10_energy_list = np.array([14.,   15.,   16.,   17.,   18.,   19.,   20.,   21.01])
+        self.proton_Xmax_list         = np.array([500., 580.,  640.,  690.,  760.,  800.,  845.,  880.]) # in g/cm^2
+        self.get_proton_Xmax          = Akima1DInterpolator(self.proton_log10_energy_list, self.proton_Xmax_list)
+
+        self.shower_lam = self.get_proton_Xmax(self.proton_log10_energy) / self.density_reg * 1.e-5 # in km
+
+
+        self.shower_pos = self.Moon_radius_km * self.r_M - self.shower_lam * self.r_CR
+        
+        #for k in range(0,self.num_events):
+        self.R_SC = np.array([0., 0., self.Moon_radius_km + self.altitude])
+        e1 = self.R_SC/np.sqrt(np.dot(self.R_SC,self.R_SC))
+        e2 = np.zeros((3,self.num_events))
+        #proj = (self.R_SC[0]*self.shower_pos[0,:] + self.R_SC[1]*self.shower_pos[1,:] + self.R_SC[2]*self.shower_pos[2,:])/(np.dot(self.R_SC,self.R_SC))
+        proj = np.einsum('i,ij->j', self.R_SC, self.shower_pos)/(np.dot(self.R_SC,self.R_SC))
+        e2 = self.shower_pos - proj*np.outer(self.R_SC, np.ones(self.num_events))
+        #e2 /= np.sqrt(e2[0,:]**2 + e2[1,:]**2 + e2[2,:]**2)
+        e2 /=  np.sqrt(np.einsum('ij,ij->j',e2,e2))
+        self.R_Tx = np.zeros((3,self.num_events))
+
+        self.snell_residual = []
+        for k in range(0,self.num_events):
+            #print 'e1[:,k], e2[:,k]', e1[:], e2[:,k]
+            def snell(x):
+                #print 'x', x
+                '''
+                if x < np.dot(self.R_SC, self.shower_pos[:,k])/np.sqrt(np.dot(self.R_SC,self.R_SC)):
+                    return np.inf
+                if x> self.Moon_radius_km:
+                    return np.inf
+                if self.Moon_radius_km**2 - x**2<0:
+                    return np.inf
+                #R_Tx = x*e1 + np.sqrt(self.Moon_radius_km**2 - x**2)*e2[:,k]
+                '''
+                R_Tx = np.sqrt(self.Moon_radius_km**2 - x**2) * e1 + x * e2[:,k]
+                #if R_Tx[0]>self.shower_pos[0,k]: 
+                #    return np.inf
+                '''You coul do this with a dot product to calculate the cosine
+                and then do sin = sqrt(1-cos^2). It might help with precision
+                near the horizon.'''
+                
+                #'''
+                c1 = np.cross(R_Tx, self.R_SC-R_Tx) 
+                c2 = np.cross(R_Tx, self.shower_pos[:,k]-R_Tx)
+                d1 = R_Tx - self.R_SC
+                d2 = R_Tx - self.shower_pos[:,k]
+                mc1 = np.sqrt(np.dot(c1, c1))
+                mc2 = np.sqrt(np.dot(c2, c2))
+                md1 = np.sqrt(np.dot(d1, d1))
+                md2 = np.sqrt(np.dot(d2, d2))
+                #val = mc1/md1 - self.n_reg * mc2/md2
+                #print x, mc2, md1, mc1/md1 * md2/mc2, self.n_reg 
+                val = mc1/md1 * md2/mc2 - self.n_reg 
+                #'''
+                
+                '''
+                d1 = np.dot(R_Tx, self.R_SC-R_Tx)
+                d2 = np.dot(R_Tx, self.shower_pos[:,k]-R_Tx)
+                c1sq = d1**2/(np.dot(R_Tx, R_Tx)*np.dot(self.R_SC-R_Tx,self.R_SC-R_Tx))
+                c2sq = d2**2/(np.dot(R_Tx, R_Tx)*np.dot(self.shower_pos[:,k]-R_Tx, self.shower_pos[:,k]-R_Tx))
+                val =  1. - np.sqrt((1.-c1sq)/(self.n_reg**2*(1.-c2sq)))
+                '''
+                return np.abs(val)
+                #print 'in Snell:' 
+                #print '\t x %1.2e'%(x)
+                #print '\t R_Tx %1.2e %1.2e %1.2e'%(R_Tx[0], R_Tx[1], R_Tx[2])
+                #print '\t mc1/md1, mc2/md2', mc1/md1/self.Moon_radius_km, mc2/md2/self.Moon_radius_km
+                #print '\t val %1.2e'%(val)
+                #return np.abs(val)
+            
+            x0 = self.shower_pos[0,k]-1.
+            x1 = self.shower_pos[0,k]
+            #res = minimize_scalar(snell, bounds=(x0, x1), method='bounded')
+            #print 'res.x', res.x
+            ''' This is probably overkil precision ''' 
+            #res = minimize(snell, res.x, tol=1.e-6)
+            #print 'self.shower_pos[0,k]-5.e-3', self.shower_pos[0,k]-5.e-3
+            res = minimize(snell, [self.shower_pos[0,k]-5.e-3], method='TNC', bounds = [(x0,x1)], tol=1.e-6)
+           # while res.fun>1.e-1:
+           #     x_guess = np.random.uniform(x0,x1)
+           #     res = minimize(snell, [x_guess], method='TNC', bounds = [(x0,x1)], tol=1.e-6)
+                
+            #print '%1.2e'%res.fun, res.success
+            self.snell_residual.append(res.fun)
+            #print 'res.x', res.x, res.success
+            #self.R_Tx[:,k] = res.x*e1 + np.sqrt(self.Moon_radius_km **2-res.x**2)*e2[:,k]
+            self.R_Tx[:,k] = np.sqrt(self.Moon_radius_km **2-res.x**2) * e1 + res.x * e2[:,k]
+
+        #dot = (self.r_CR[0,:]*(self.R_Tx - self.shower_pos)[0,:]) + (self.r_CR[1,:]*(self.R_Tx - self.shower_pos)[1,:]) + (self.r_CR[2,:]*(self.R_Tx - self.shower_pos)[2,:])
+        '''
+        Cut events from the trigger if the snell residuals suck.
+        This usually happens at nadir or by the horizon.
+        '''
+        self.snell_residual = np.array(self.snell_residual)
+        '''
+        This is the unit vector pointing from the shower to the refraction
+        point on the surface. 
+        '''
+        self.u_reg = self.R_Tx - self.shower_pos
+        self.dist_reg = np.sqrt(np.einsum('ij,ij->j', self.u_reg, self.u_reg)) 
+        self.u_reg /= self.dist_reg
+        
+        '''a
+        The cosine of the view angle is the dot product of the direction of
+        propagation (-r_CR) and the direction from the shower to the 
+        refraction point.
+        '''
+        dot = np.einsum('ij,ij->j',-self.r_CR, self.u_reg) 
+        self.view_angle_rad = np.arccos(dot)
+
+        '''
+        The polarization vector is the projected direction of the shower 
+        (times -1 since the charge excess is negative) given by r_CR and the
+        direction of the radio propagation from the shower to the refraction  
+        point on the surface
+        '''        
+        self.pol_vec_reg= -self.r_CR - dot * self.u_reg
+        self.pol_vec_reg /= np.sqrt(np.einsum('ij,ij->j',self.pol_vec_reg, self.pol_vec_reg))
+        
+        '''
+        These values are needed to estimate the transition coefficient. 
+        The first thing needed are the polarization vector component 
+        (in regolith) parallel and orthogonal to the normal to the surface 
+        '''
+        self.u_pol_vec_perp = self.R_Tx/np.sqrt(np.einsum('ij,ij->j', self.R_Tx, self.R_Tx))
+        
+        self.u_pol_vec_para = np.zeros(self.u_pol_vec_perp.shape)
+        self.u_pol_vec_para[0,:] = self.u_reg[1,:]*self.u_pol_vec_perp[2,:] - self.u_reg[2,:]*self.u_pol_vec_perp[1,:] 
+        self.u_pol_vec_para[1,:] = self.u_reg[2,:]*self.u_pol_vec_perp[0,:] - self.u_reg[0,:]*self.u_pol_vec_perp[2,:]
+        self.u_pol_vec_para[2,:] = self.u_reg[0,:]*self.u_pol_vec_perp[1,:] - self.u_reg[1,:]*self.u_pol_vec_perp[0,:]
+        self.u_pol_vec_para /= np.sqrt(np.einsum('ij,ij->j', self.u_pol_vec_para, self.u_pol_vec_para))
+
+        '''
+        The polarization vector after refraction is given by the polarization
+        vector prior to refraction projected in the direction from the 
+        refraction point to the spacecraft.
+        ref stands for refracted
+        '''
+        self.u_orb = np.outer(self.R_SC, np.ones(self.num_events)) - self.R_Tx
+        self.u_orb /= np.sqrt(np.einsum('ij,ij->j', self.u_orb, self.u_orb))
+
+        self.u_pol_vec_perp_ref = self.u_pol_vec_perp.copy()
+        self.u_pol_vec_perp_ref -= np.einsum('ij,ij->j',self.u_pol_vec_perp, self.u_orb)*self.u_orb
+        self.u_pol_vec_perp_ref /= np.sqrt(np.einsum('ij,ij->j',self.u_pol_vec_perp_ref, self.u_pol_vec_perp_ref))
+
+        self.u_pol_vec_para_ref = self.u_pol_vec_para.copy()
+        self.u_pol_vec_para_ref -= np.einsum('ij,ij->j',self.u_pol_vec_para, self.u_orb)*self.u_orb
+        self.u_pol_vec_para_ref /= np.sqrt(np.einsum('ij,ij->j',self.u_pol_vec_para_ref, self.u_pol_vec_para_ref))
+
+        
+        
+        '''
+        The projection factors for the electric field magnitude in the 
+        directions parallel and sperpendicular to the surface.
+        '''
+        #self.e_perp = np.einsum('ij,ij->j', self.u_reg, self.u_pol_vec_perp)
+        #self.e_para = np.einsum('ij,ij->j', self.u_reg, self.u_pol_vec_para)
+        self.e_perp = np.einsum('ij,ij->j', self.pol_vec_reg, self.u_pol_vec_perp)
+        self.e_para = np.einsum('ij,ij->j', self.pol_vec_reg, self.u_pol_vec_para)
+        
+        '''
+        The modified transmission coefficients (See Lehtinen+ 2003, FORTE 
+        satelline constraints... Appendix 2).
+        '''
+        
+        #cos_ref = np.einsum('ij,ij->j',self.u_pol_vec_perp, np.outer(self.R_SC, np.ones(self.num_events)) - self.R_Tx)
+        #cos_ref /= np.sqrt(np.einsum('ij,ij->j',np.outer(self.R_SC, np.ones(self.num_events)) - self.R_Tx, np.outer(self.R_SC, np.ones(self.num_events)) - self.R_Tx))
+
+        #cos_inc = np.einsum('ij,ij->j',self.u_pol_vec_perp, self.R_Tx - self.shower_pos )
+        #cos_inc /= np.sqrt(np.einsum('ij,ij->j', self.R_Tx - self.shower_pos, self.R_Tx - self.shower_pos))
+
+        self.cos_ref  =  np.einsum('ij,ij->j',self.R_Tx, np.outer(self.R_SC, np.ones(self.num_events))-self.R_Tx)
+        self.cos_ref /= np.sqrt(np.einsum('ij,ij->j',self.R_Tx, self.R_Tx)*np.einsum('ij,ij->j',np.outer(self.R_SC, np.ones(self.num_events))-self.R_Tx, np.outer(self.R_SC, np.ones(self.num_events))-self.R_Tx))
+        
+        self.cos_inc  = np.einsum('ij,ij->j',-self.R_Tx, self.shower_pos -self.R_Tx)
+        self.cos_inc /= np.sqrt(np.einsum('ij,ij->j',-self.R_Tx, -self.R_Tx) * np.einsum('ij,ij->j',self.shower_pos -self.R_Tx,self.shower_pos -self.R_Tx))
+ 
+
+        self.th_inc_rad = np.arccos(self.cos_inc)        
+
+        self.e_perp *= 2.*self.cos_ref/(self.n_reg * self.cos_inc + self.cos_ref)
+        self.e_para *= 2.*self.cos_ref/(self.n_reg * self.cos_ref + self.cos_inc)
+        
+        '''
+        Finally, these are divided by the distance from the transmission point 
+        to the spacecraft. So all we need to do to get the electric field 
+        vector at the antenna is to multiply it by these coefficients.
+        '''
+        self.R_surf_to_SC = np.sqrt(np.einsum('ij,ij->j',np.outer(self.R_SC, np.ones(self.num_events)) - self.R_Tx, np.outer(self.R_SC, np.ones(self.num_events)) - self.R_Tx))
+
+        self.e_perp *= 1./(self.R_surf_to_SC*1.e3) # in units of 1/m
+        self.e_para *= 1./(self.R_surf_to_SC*1.e3) # in units of 1/m
+        
+        self.pol_vec_ref = self.e_para * self.u_pol_vec_para_ref + self.e_perp * self.u_pol_vec_perp_ref
+        self.pol_vec_ref /= np.sqrt(np.einsum('ij,ij->j', self.pol_vec_ref, self.pol_vec_ref))
+        
